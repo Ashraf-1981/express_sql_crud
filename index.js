922e16760462f633d61301dea095756c3bb44b6a
acap <acap@example.com>
2026-09-20 01:17:07 +0800

    Task 8 done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("VERSION 2 LOADED");
+
 const express = require('express');
 const mysql2 = require('mysql2/promise');
 const ejs = require('ejs');
@@ -35,10 +37,59 @@ const dbConnection = mysql2.createPool(dbConfig);
 
 // Students add their routes here.
 
+// create 'C' and a blank form + cuisine route
+app.get("/cuisines/create", function (req, res) {
+    res.render('create-cuisines');
+});
 
-// ==========================================
-// Start Server
-// ==========================================
+// 'C' procees a form and Insert a new cuisine into the database.
+app.post('/cuisines/create', async function (req, res) {
+    const { name } = req.body;
+    const sql = "INSERT INTO cuisines (name) VALUES (?)";
+    await dbConnection.execute(sql, [name]);
+    res.redirect('/cuisines');
+});
+
+// List 'R' all cuisines route
+app.get('/cuisines', async function (req, res) {
+    const [rows] = await dbConnection.execute("SELECT * FROM cuisines");
+    res.render('list-cuisines', {
+        cuisines: rows
+    });
+});
+
+// 'U' show edit form
+app.get('/cuisines/:id/edit', async function (req, res) {
+    const id = req.params.id;
+    const [rows] = await dbConnection.execute(
+        "SELECT * FROM cuisines WHERE cuisine_id = ?",
+        [id]
+    );
+    res.render('edit-cuisines', {
+        cuisine: rows[0]
+    });
+});
+
+// 'U' save changes
+app.post('/cuisines/:id/edit', async function (req, res) {
+    const id = req.params.id;
+    const { name } = req.body;
+    const sql = "UPDATE cuisines SET name = ? WHERE cuisine_id = ?";
+    await dbConnection.execute(sql, [name, id]);
+    res.redirect('/cuisines');
+});
+
+// 'D' delete a cuisine
+app.post('/cuisines/:id/delete', async function (req, res) {
+     console.log("DELETE ROUTE HIT, id =", req.params.id);
+    const id = req.params.id;
+    const sql = "DELETE FROM cuisines WHERE cuisine_id = ?";
+    await dbConnection.execute(sql, [id]);
+    res.redirect('/cuisines');
+});
+
+
+
 
 app.listen(port, function () {
     console.log(`Server has started on port ${port}`);
